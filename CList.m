2882782ae1a26dde248e155801a5107e467ee1cf
Vic <dlqranger@gmail.com>
2022-01-08 23:43:45 +0800

    v1.0

--- CList.m
+++ CList.m
@@ -1,57 +1,64 @@
 classdef CList < handle
-% 定义了一个（有序的）列表
-% list = CList; 定义一个空的队列对象
-% list = CList(c); 定义队列对象，并用c初始化q，当c为cell时，c的元素为栈的数据，
-%    否则c本身为栈的第一个数据
+% A List
+% list = CList(); Construct an empty list object
+% list = CList(c); Construct a list: if c is a cell, then the list is
+%     initialized with c's elements; otherwise c becomes the first element
 %
-% 支持操作：
-%     sz = CList.size() 返回队列内元素个数
-%     y = CList.empty() 返回队列是否为空
-%     CList.pushtofront(el) 将新元素el压入列表头
-%     CList.pushtorear(el) 将新元素el压入列表尾部
-%     el = CList.popfront()  弹出列表头部元素，用户需自己确保队列非空
-%     el = CList.poprear() 弹出列表尾部元素，用户需自己确保列表非空
-%     el = CList.front() 返回队首元素，用户需自己确保队列非空
-%     el = CList.back() 返回队尾元素，用户需自己确保队列非空
-%     CList.remove(k) 删除第k个元素，如果k为负的，则从尾部开始算  
-%     CList.removeall() 删除队列所有元素
-%     CList.add(el, k) 插入元素el到第k个位置，如果k为负的，则从结尾开始算
-%     CList.contains(el) 检查el是否出现在列表中，如果出现，返回第一个下标
-%     CList.get(k) 返回列表制定位置的元素，如果k为负的，则从末尾开始算
-%     CList.sublist(from, to) 返回列表中从from到to（左开右闭）之间的视图
-%     CList.content() 返回列表的数据，以一维cells数组的形式返回。
-%     CList.toarray() = CList.content() content的别名
-%
-% See also CStack
+% Methods：
+%     sz = CList.size() Return size
+%     y = CList.empty() Test whether container is empty
+%     c = CList.capacity() Return current capacity(buffer size)
+%     CList.clear() Clear content
+%     CList.push_front(el) Insert element at beginning
+%     CList.push_back(el) Add element at the end
+%     el = CList.pop_front() Delete first element
+%     el = CList.pop_back() Delete last element
+%     el = CList.front() Return first element
+%     el = CList.back() Return last element
+%     CList.erase(k) Erase the k-th element, if k is negative, then erase 
+%     the reverse k-th element
+%     CList.insert(el, k) Insert a new element at the k-th position, k can
+%     be negative
+%     CList.content() Return a 1-D cell array containing all data
+%     numel(instance) Return size
+%     size(instance) Return size
 %
 % copyright: zhangzq@citics.com, 2010.
 % url: http://zhiqiang.org/blog/tag/matlab
+%
+% Modified by VictoriousRaptor
+% See https://github.com/VictoriousRaptor/MATLAB-Datastructures
 
     properties (Access = private)
-        buffer      % 一个cell数组，保存栈的数据
-        beg         % 队列起始位置
-        len         % 队列的长度
-        cap    % 栈的容量，当容量不够时，容量扩充为2倍。
+        buffer      % A cell array
+        beg         % beginning pos of the list
+        last        % last pos, empty, next available pos
+        len         % length
+        cap         % capacity
     end
 
     methods (Access = public)
+        % if c is cell array then use its elements, otherwise c will be the first element
         function self = CList(c)
             starting_capacity = 5;
             if nargin >= 1 && iscell(c)
-                self.buffer = [c(:); cell(numel(c), 1)];
+                self.buffer = [c(:), cell(1, numel(c))];
                 self.beg = 1;
                 self.len = numel(c);
-                self.cap = 2*numel(c);
+                self.cap = self.len;
+                self.last = self.len + 1;
             elseif nargin >= 1
-                self.buffer = cell(starting_capacity, 1);
+                self.buffer = cell(1, starting_capacity);
                 self.buffer{1} = c;
                 self.beg = 1;
+                self.last = 1;
                 self.len = 1;
                 self.cap = starting_capacity;                
             else
-                self.buffer = cell(starting_capacity, 1);
+                self.buffer = cell(1, starting_capacity);
                 self.cap = starting_capacity;
                 self.beg = 1;
+                self.last = 1;
                 self.len = 0;
             end
         end
@@ -72,22 +79,20 @@ classdef CList < handle
             b = (self.len == 0);
         end
 
-        function clear(self) % 清空列表
+        function clear(self)
             self.len = 0;
             self.beg = 1;
+            self.last = 2;
         end
         
-        function push_back(self, el) % 压入新元素到队尾
+        function push_back(self, el)  % push a new element to the back
             self.increase_capacity();
-            if self.beg + self.len  <= self.cap
-                self.buffer{self.beg+self.len} = el;
-            else
-                self.buffer{self.beg+self.len-self.cap} = el;
-            end
+            self.buffer{self.last} = el;
             self.len = self.len + 1;
+            self.last = mod(self.beg + self.len, self.cap);
         end
         
-        function push_front(self, el) % 压入新元素到队头
+        function push_front(self, el)  % push a new element to the front
             self.increase_capacity();
             self.beg = self.beg - 1;
             if self.beg == 0
@@ -95,107 +100,120 @@ classdef CList < handle
             end
             self.buffer{self.beg} = el;
             self.len = self.len + 1;
+            self.last = mod(self.beg + self.len, self.cap);
         end
         
-        function el = pop_front(self) % 弹出队首元素
+        function el = pop_front(self)  % pop the first element
             el = self.buffer(self.beg);
-            self.beg = self.beg + 1;
             self.len = self.len - 1;
+            self.beg = self.beg + 1;
             if self.beg > self.cap
                 self.beg = 1;
             end
         end
         
-        function el = pop_back(self) % 弹出队尾元素
+        function el = pop_back(self)  % pop the last element
             tmp = self.beg + self.len;
             if tmp > self.cap
                 tmp = tmp - self.cap;
             end
             el = self.buffer(tmp);
             self.len = self.len - 1;
+            self.last = mod(self.beg + self.len, self.cap);
         end
         
-        function el = front(self) % 返回队首元素
+        function el = front(self)  % get the first element
             try
-                el = self.buffer{self.beg};
+                el = self.buffer{1, self.beg};
             catch ME
                 throw(ME.messenge);
             end
         end
         
-        function el = back(self) % 返回队尾元素
+        function el = back(self)  % get the last element
             try
-                tmp = self.beg + self.len - 1;
-                if tmp >= self.cap
-                    tmp = tmp - self.cap;
-                end
-                el = self.buffer(tmp);
+                el = self.buffer{1, self.get_index(self.len)};
             catch ME
                 throw(ME.messenge);
             end            
         end
         
-        % 删除第k个元素，k可以为负的，表示从尾部开始算
+        % Erase the k-th element, if k is negative, then erase the reverse k-th
+        % element
+        % TODO erase range
         function erase(self, k)
-            if nargin == 2
-                id = self.get_index(k);
-
-                self.buffer{id} = [];
-                self.len = self.len - 1;
-                self.cap = self.cap - 1;
-
-                % 删除元素后，需要重新调整beg的位置值
-                if id < self.beg
-                    self.beg = self.beg - 1;
-                end
+            if self.empty()
+                error('CList: erasing element in a empty list')
+            elseif k == 0
+                error('CList: erasing the 0-th elementt')
             end
-        end
-        
-        % 插入新元素el到第k个元素之前，k可以为负数
-        function insert(self, el, k)
-            self.increase_capacity();
             id = self.get_index(k);
-            
-            if k > 0 % 插入在第id个元素之前
-                self.buffer = [self.buffer(1:id-1); el; self.buffer(id:end)];
-                if id < self.beg
-                    self.beg = self.beg + 1;
+            if self.last < self.beg
+               for i = id:self.last - 2
+                   self.buffer{i} = self.buffer{i + 1};
+               end
+            else
+                for i = self.beg + 1:self.id
+                    self.buffer{i} = self.buffer{i - 1};
                 end
+                self.beg = self.beg + 1;
             end
+            self.len = self.len - 1;
+            self.last = mod(self.beg + self.len, self.cap);
         end
         
-        % 依次显示队列元素
+        % Insert new elements before the element at the specified position k
+        % k can be negative (reverse index)
+        function insert(self, el, k)
+            if k == 0
+                error('CList: accessing the 0-th elementt')
+            end
+            self.increase_capacity();
+            if k < 0
+                k = self.len - abs(k) + 1;
+            end
+            id = self.get_index(k);
+            self.buffer = [self.buffer(1:id - 1), el, self.buffer(id:end - 1)];
+            if id < self.beg
+                self.beg = self.beg + 1;
+            end
+            self.len = self.len + 1;
+            self.last = mod(self.beg + self.len, self.cap);
+        end
+        
+        % Display elements
         function disp(self)
-            if self.size()
-                rear = self.beg + self.len - 1;
-                if rear <= self.cap
-                    for i = self.beg : rear
-                        disp([num2str(i - self.beg + 1) '-th element of the stack:']);
+            if ~self.empty()
+                if self.last > self.beg
+                    for i = self.beg:self.last - 1
+                        disp([num2str(i - self.beg + 1) '-th element of the list:']);
                         disp(self.buffer{i});
                     end
                 else
                     for i = self.beg : self.cap
-                        disp([num2str(i - self.beg + 1) '-th element of the stack:']);
+                        disp([num2str(i - self.beg + 1) '-th element of the list:']);
                         disp(self.buffer{i});
                     end     
-                    for i = 1 : rear
-                        disp([num2str(i + self.cap - self.beg + 1) '-th element of the stack:']);
+                    for i = 1 :self.last - 1  % loop
+                        disp([num2str(i + self.cap - self.beg + 1) '-th element of the list:']);
                         disp(self.buffer{i});
                     end
                 end
             else
-                disp('The queue is empty');
+                disp('This list is empty');
             end
         end
         
         
-        % 获取列表的数据内容
+        % Return all content
         function c = content(self)
-            rear = self.beg + self.len - 1;
-            if rear <= self.cap
-                c = self.buffer(self.beg:rear);                    
+            if self.empty()
+                c = {};
+            end
+            if self.last > self.beg
+                c = self.buffer(self.beg:self.last - 1);                    
             else
-                c = self.buffer([self.beg:self.cap 1:rear]);
+                c = self.buffer([self.beg:self.cap, 1:self.last - 1]);
             end
         end
         
@@ -203,10 +221,10 @@ classdef CList < handle
     
     methods (Access = private)
         
-        % getindex(k) 返回第k个元素在buffer的下标位置
+        % get the buffer index of the k-th element
         function id = get_index(self, k)
             if k > 0
-                id = self.beg + k;
+                id = self.beg + k - 1;
             else
                 id = self.beg + self.len + k;
             end     
@@ -215,23 +233,18 @@ classdef CList < handle
                 id = id - self.cap;
             end
         end
-        
-        % 当buffer的元素个数接近容量上限时，将其容量扩充一倍。
-        % 此时旋转列表，使得从1开始。整个列表至少有两个以上空位。
+
         function increase_capacity(self)
-            if self.len >= self.cap - 1
-                sz = self.len;
-                if self.beg + sz - 1 <= self.cap
-                    self.buffer(1:sz) = self.buffer(self.beg:self.beg+sz-1);                    
-                else
-                    self.buffer(1:sz) = self.buffer([self.beg:self.cap, ...
-                        1:sz-(self.cap-self.beg+1)]);
+            if self.len == self.cap - 1
+                self.buffer = [self.buffer, cell(1, self.cap)];
+                if self.beg > 2  %  must be a loop
+                    self.buffer(self.cap + 1, self.cap + self.beg - 1) = self.buffer(1:self.beg - 2);
                 end
-                self.buffer(sz+1:self.cap*2) = cell(self.cap*2-sz, 1);
-                self.cap = 2*self.cap;
-                self.beg = 1;
+                self.cap = self.cap * 2;
+                self.last = mod(self.beg + self.len, self.cap);
             end
         end
-    end % private methods
+        
+    end
 
 end
